478c21614c893034f6f2dac2cee8423eb154d426
Juuhyunn <ahnju96@gmail.com>
2021-10-01 12:46:00 +0900

    1, October

--- test1/hello.py
+++ test1/hello.py
@@ -2,19 +2,29 @@ import xlwings as xw
 
 
 def main():
+    # add()
+    vlookup()
+
+
+def add():
     a = xw.Book.caller()
     sheet = a.sheets[0]
     i = 1
     sheet[f'A{i}'].value = i
-
-    # print(sheet[f'A{i}'].value + sheet['A2'].value)
-    # if target.value == "Hello xlwings!" :
-    #     target.value = "Bye xlwings!"
-    # else :
-    #     target.value = "Hello xlwings!"
-    #
     for i in range(4):
         sheet[f'C{i+1}'].value = sheet[f'A{i+1}'].value + sheet[f'B{i+1}'].value
+
+
+def vlookup():
+    a = xw.Book.caller()
+    sheet = a.sheets[0]
+    for i in range(10):
+        if sheet[f'A{i+1}'].value == sheet[f'B{i+1}'].value:
+            sheet[f'C{i+1}'].value = True
+        else:
+            sheet[f'C{i+1}'].value = False
+
+
 
 
 
